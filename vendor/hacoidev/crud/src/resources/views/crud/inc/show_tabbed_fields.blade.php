955cbcae54fa25cdf83cdb0c8eb3c8ec6b4645d3
Võ Định <vodinh2000ht@gmail.com>
2023-05-29 21:25:16 +0700

    update

--- vendor/hacoidev/crud/src/resources/views/crud/inc/show_tabbed_fields.blade.php
+++ vendor/hacoidev/crud/src/resources/views/crud/inc/show_tabbed_fields.blade.php
@@ -1,9 +1,11 @@
 @php
     $horizontalTabs = $crud->getTabsType()=='horizontal' ? true : false;
 
-    if ($errors->any() && array_key_exists(array_keys($errors->messages())[0], $crud->getCurrentFields()) &&
-        array_key_exists('tab', $crud->getCurrentFields()[array_keys($errors->messages())[0]])) {
-        $tabWithError = ($crud->getCurrentFields()[array_keys($errors->messages())[0]]['tab']);
+    if(isset($errors)) {
+        if ($errors->any() && array_key_exists(array_keys($errors->messages())[0], $crud->getCurrentFields()) &&
+            array_key_exists('tab', $crud->getCurrentFields()[array_keys($errors->messages())[0]])) {
+            $tabWithError = ($crud->getCurrentFields()[array_keys($errors->messages())[0]]['tab']);
+        }
     }
 @endphp
 
